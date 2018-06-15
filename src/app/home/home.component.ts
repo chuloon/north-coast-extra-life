814421ad8743033866a5706bf765d26beb61cc68
ben <ben.ng@outlook.com>
2018-06-15 16:08:09 -0400

    got profiles working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,6 +11,12 @@ export class HomeComponent implements OnInit {
   @ViewChild(PerfectScrollbarComponent) ps: PerfectScrollbarComponent;
   showMemberInfo: boolean = false;
 
+  selectedMember;
+  selectedName;
+  selectedHobbies;
+  selectedGames;
+  selectedHometown;
+
   platinumSponsors = [
     {
       name: 'tabletop',
@@ -28,33 +34,24 @@ export class HomeComponent implements OnInit {
     {
       id: 'jessica',
       name: 'Jess Coleman',
-      hobbies: [
-        'Creative Writing', 'Making Clay Dice Dragons', 'Sketching Characters'
-      ],
-      games: [
-        'Ticket to Ride', 'Dominion', 'Betrayal at the House on the Hill', 'DnD 5e', 'Sims', 'Rollercoaster Tycoon 3', 'Civ IV'
-      ]
+      hometown: 'Cleveland, OH',
+      hobbies: "Creative Writing, Making Clay Dice Dragons, and Sketching Characters",
+      games: "Ticket to Ride, Dominion, Betrayal at the House on the Hill, DnD 5e, Sims, Rollercoaster Tycoon 3, and Civ IV"
     },
     {
       id: 'jessica',
       name: 'Jess Coleman',
-      hobbies: [
-        'Creative Writing', 'Making Clay Dice Dragons', 'Sketching Characters'
-      ],
-      games: [
-        'Ticket to Ride', 'Dominion', 'Betrayal at the House on the Hill', 'DnD 5e', 'Sims', 'Rollercoaster Tycoon 3', 'Civ IV'
-      ]
+      hometown: 'Cleveland, OH',
+      hobbies: "Creative Writing, Making Clay Dice Dragons, and Sketching Characters",
+      games: "Ticket to Ride, Dominion, Betrayal at the House on the Hill, DnD 5e, Sims, Rollercoaster Tycoon 3, and Civ IV"
     },
     {
       id: 'jessica',
       name: 'Jess Coleman',
-      hobbies: [
-        'Creative Writing', 'Making Clay Dice Dragons', 'Sketching Characters'
-      ],
-      games: [
-        'Ticket to Ride', 'Dominion', 'Betrayal at the House on the Hill', 'DnD 5e', 'Sims', 'Rollercoaster Tycoon 3', 'Civ IV'
-      ]
-    },
+      hometown: 'Cleveland, OH',
+      hobbies: "Creative Writing, Making Clay Dice Dragons, and Sketching Characters",
+      games: "Ticket to Ride, Dominion, Betrayal at the House on the Hill, DnD 5e, Sims, Rollercoaster Tycoon 3, and Civ IV"
+    }
   ];
 
   constructor(private renderer: Renderer2) { }
@@ -75,6 +72,16 @@ export class HomeComponent implements OnInit {
 
     if(value) this.renderer.addClass(target, 'show')
     else this.renderer.removeClass(target, 'show');
+
+    this.resetAboutMe(target);
+  }
+
+  resetAboutMe = (target) => {
+    this.renderer.removeClass(target, 'hide');
+
+    this.selectedGames = [];
+    this.selectedHobbies = [];
+    this.selectedName = "";
   }
 
   ctaClick = (action: string) => {
@@ -87,5 +94,32 @@ export class HomeComponent implements OnInit {
 
   getThumbnailPath = (id: string) => {
     return "../../assets/images/members/" + id + ".jpg"
+  }
+
+  memberClick = (member) => {
+    this.showMemberInfo = true;
+    document.getElementById("about-us").classList.remove("show");
+
+    this.selectedName = member.name;
+    this.selectedHometown = member.hometown;
+    this.selectedGames = member.games;
+    this.selectedHobbies = member.hobbies;
+
+    this.selectedMember = member;
+
+    setTimeout(() => {
+      document.getElementById("about-us").classList.add("hide");
+      document.getElementById(member.id + '-thumbnail').classList.add("selected");
+    }, 300);
+  }
+
+  closeMember = () => {
+    document.getElementById("about-us").classList.remove("hide");
+    document.getElementById(this.selectedMember.id + '-thumbnail').classList.remove("selected");
+
+    setTimeout(() => {
+      this.showMemberInfo = false;
+      document.getElementById("about-us").classList.add("show");
+    }, 300);
   }
 }
