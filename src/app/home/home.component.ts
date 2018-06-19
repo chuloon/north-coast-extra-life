2e07338d857b785ec86ea42309a37fe005ae1ad7
ben <ben.ng@outlook.com>
2018-06-18 21:08:39 -0400

    added ben and steven info

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -39,18 +39,18 @@ export class HomeComponent implements OnInit {
       games: "Ticket to Ride, Dominion, Betrayal at the House on the Hill, DnD 5e, Sims, Rollercoaster Tycoon 3, and Civ IV"
     },
     {
-      id: 'jessica',
-      name: 'Jess Coleman',
-      hometown: 'Cleveland, OH',
-      hobbies: "Creative Writing, Making Clay Dice Dragons, and Sketching Characters",
-      games: "Ticket to Ride, Dominion, Betrayal at the House on the Hill, DnD 5e, Sims, Rollercoaster Tycoon 3, and Civ IV"
+      id: 'ben',
+      name: 'Ben Ng',
+      hometown: 'Cincinnati, OH',
+      hobbies: "Cosplay, sand volleyball, and video games",
+      games: "Final Fantasy XIV, League of Legends, Horizon Zero Dawn, and Overwatch"
     },
     {
-      id: 'jessica',
-      name: 'Jess Coleman',
-      hometown: 'Cleveland, OH',
-      hobbies: "Creative Writing, Making Clay Dice Dragons, and Sketching Characters",
-      games: "Ticket to Ride, Dominion, Betrayal at the House on the Hill, DnD 5e, Sims, Rollercoaster Tycoon 3, and Civ IV"
+      id: 'steven',
+      name: 'Steven Herron',
+      hometown: 'Chippewa, PA',
+      hobbies: "Scouting, video games, role playing, live action role playing, recreational shooting/hunting",
+      games: "Dnd 3.5/5.0, Star Wars d20, Ark, Anything Elder scrolls or fallout, Legend of Zelda series, GTA series"
     }
   ];
 
@@ -89,6 +89,11 @@ export class HomeComponent implements OnInit {
       case 'join':
       window.open('https://www.extra-life.org/index.cfm?fuseaction=donorDrive.team&teamID=39974', '_blank');
       break;
+      case 'sponsor':
+      window.open('mailto:jmcoleman.568@gmail.com?subject=New Sponsor Inquiry');
+      break;
+      case 'volunteer':
+      window.open('mailto:jmcoleman.568@gmail.com?subject=Volunteer Application');
     }
   }
 
